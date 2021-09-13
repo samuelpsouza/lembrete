77b0ee05b362e418f3e9da64dd1b80ef04175407
Samuel Souza <samuelsouza.contact@gmail.com>
2021-09-12 22:47:56 -0300

    setting mongoose connection

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,21 +3,18 @@ module.exports = {
         username: process.env.DEV_DB_USERNAME,
         password: process.env.DEV_DB_PASSWORD,
         database: process.env.DEV_DB_DATABASE,
-        host: process.env.DEV_DB_HOSTNAME,
-        dialect: 'postgres',
+        host: process.env.DEV_DB_HOSTNAME
     },
     test: {
         username: process.env.TEST_DB_USERNAME,
         password: process.env.TEST_DB_PASSWORD,
         database: process.env.TEST_DB_DATABASE,
-        host: process.env.DB_HOSTNAME,
-        dialect: 'postgres',
+        host: process.env.DB_HOSTNAME
     },
     production: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.PROD_DB_DATABASE,
-        host: process.env.DB_HOSTNAME,
-        dialect: 'postgres',
+        host: process.env.DB_HOSTNAME
     }
 };
